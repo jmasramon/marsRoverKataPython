391f4c65b5358a2951a6cbebdd4f8f60cf5cc105
Jordi Masramon <jordi.masramon@gmail.com>
2015-06-21 23:41:10 +0200

    some more work done. Obstacle detection pending

--- test_rover.py
+++ test_rover.py
@@ -17,17 +17,20 @@ from pyspecs import given, when, then, and_, the
 # Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle,
 # the rover moves up to the last possible point and reports the obstacle.
 
+# TODO: make tests independent of one another resetting the position and orientation (the test framework does not have a before()
+# TODO: abstract common control structures with higher order functions
+# TODO: separate tests from productions code
+# TODO: refactor tests
+
 def go_east(position):
     return {'x': position['x'] + 1, 'y': position['y']}
-
 
 def go_west(position):
     return {'x': position['x'] - 1, 'y': position['y']}
 
-
 def go_north(position):
+    # print('entered go_north')
     return {'x': position['x'], 'y': position['y'] + 1}
-
 
 def go_south(position):
     return {'x': position['x'], 'y': position['y'] - 1}
@@ -35,27 +38,65 @@ def go_south(position):
 def turn_east():
     return 'E'
 
-
 def turn_west():
     return 'W'
-
 
 def turn_north():
     return 'N'
 
-
 def turn_south():
     return 'S'
+
+class Navigator:
+    # map = ''
+    map_size = 0
+
+    # def setMap(self, lowerLefCorner, upperRightCorner):
+    def setMap(self, map_size):
+        # self.map = (lowerLefCorner, upperRightCorner)
+        # self.map_size = upperRightCorner[0]
+        self.map_size = map_size
+
+    def go_east(self, position):
+        # print('entered navigator.go_north')
+        temp_position = go_east(position)
+        if temp_position['x'] > self.map_size:
+            temp_position['x'] = -(temp_position['x']-1)
+        return temp_position
+
+    def go_west(self, position):
+        # print('entered navigator.go_north')
+        temp_position = go_west(position)
+        if temp_position['x'] < -self.map_size:
+            temp_position['x'] = -(temp_position['x']+1)
+        return temp_position
+
+    def go_north(self, position):
+        # print('entered navigator.go_north')
+        temp_position = go_north(position)
+        if temp_position['y'] > self.map_size:
+            temp_position['y'] = -(temp_position['y']-1)
+        return temp_position
+
+    def go_south(self, position):
+        # print('entered navigator.go_north')
+        temp_position = go_south(position)
+        if temp_position['y'] < -self.map_size:
+            temp_position['y'] = -(temp_position['y']+1)
+        return temp_position
+
 
 
 class Rover:
     position = ''
     orientation = ''
     orders = ''
+    navigator = ''
 
-    def __init__(self, initial_position, initial_orientation):
+    def __init__(self, initial_position, initial_orientation, navigator = ''):
         self.position = initial_position
         self.orientation = initial_orientation
+        self.navigator = navigator
 
     def set_orders(self, new_orders):
         self.orders = new_orders
@@ -71,11 +112,19 @@ class Rover:
                       'bN': go_south, 'bS': go_north, 'bE': go_west, 'bW': go_east,
                       'lN': turn_west, 'lS': turn_east, 'lE': turn_north, 'lW': turn_south,
                       'rN': turn_east, 'rS': turn_west, 'rE': turn_south, 'rW': turn_north}
+
         for order in self.orders:
-            if order in ('l','r'):
+            if order in ('f','b'):
+                if not self.navigator:
+                    # print('navigator not detected')
+                    self.position = order_dict[order + self.orientation](self.position)
+                else:
+                    # print('navigator detected')
+                    # print(getattr(self.navigator, (order_dict[order + self.orientation]).__name__))
+                    self.position = getattr(self.navigator, (order_dict[order + self.orientation]).__name__)(self.position)
+            elif order in ('l', 'r'):
                 self.orientation = order_dict[order + self.orientation]()
-            elif order in ('f', 'b'):
-                self.position = order_dict[order + self.orientation](self.position)
+
 
 
 cardinal_points = ('N', 'S', 'E', 'W')
@@ -125,28 +174,28 @@ with given.a_rover:
         with and_.the_rover_should_accept_f_command_and_go_forward:
             rover.set_orders('f')
             rover.run_orders()
-            # print(rover.getPosition())
+            print(rover.get_position())
             the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': 1})).should.be(True)
             the(rover.get_orientation()).should.be('N')
 
         with and_.the_rover_should_accept_b_command_and_go_backward:
             rover.set_orders('b')
             rover.run_orders()
-            # print(rover.getPosition())
+            print(rover.get_position())
             the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': 0})).should.be(True)
             the(rover.get_orientation()).should.be('N')
 
         with and_.the_rover_should_accept_l_command_and_orient_left:
             rover.set_orders('l')
             rover.run_orders()
-            # print(rover.get_position())
+            print(rover.get_position())
             the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': 0})).should.be(True)
             the(rover.get_orientation()).should.be('W')
 
         with and_.the_rover_should_accept_r_command_and_orient_right:
             rover.set_orders('r')
             rover.run_orders()
-            # print(rover.get_position())
+            print(rover.get_position())
             the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': 0})).should.be(True)
             the(rover.get_orientation()).should.be('N')
 
@@ -154,21 +203,50 @@ with given.a_rover:
         with then.the_rover_should_do_as_commanded:
             rover.set_orders('flf')
             rover.run_orders()
-            # print(rover.get_position())
+            print(rover.get_position())
             the(is_same_dictionary(rover.get_position(), {'x': -1, 'y': 1})).should.be(True)
             the(rover.get_orientation()).should.be('W')
 
             rover.set_orders('brb')
             rover.run_orders()
-            # print(rover.get_position())
+            print(rover.get_position())
             the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': 0})).should.be(True)
             the(rover.get_orientation()).should.be('N')
 
             rover.set_orders('flflflfl')
             rover.run_orders()
-            # print(rover.get_position())
+            print(rover.get_position())
             the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': 0})).should.be(True)
             the(rover.get_orientation()).should.be('N')
 
-    # with when.supplied_with_a_map:
-    #     with then.the_rover_should_wrap_from_the_edges:
+    with when.supplied_with_a_navigator:
+        navigator = Navigator()
+        # navigator.setMap((-3,-3), (3,3))
+        MAP_SIZE = 3
+        navigator.setMap(MAP_SIZE)
+        rover = Rover(starting_point, initial_direction, navigator)
+
+        with and_.the_rover_should_wrap_from_the_upper_edge:
+            rover.set_orders('ffff')
+            rover.run_orders()
+            print(rover.get_position())
+            the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': -MAP_SIZE})).should.be(True)
+
+        with and_.the_rover_should_wrap_from_the_lower_edge:
+            rover.set_orders('b')
+            rover.run_orders()
+            print(rover.get_position())
+            the(is_same_dictionary(rover.get_position(), {'x': 0, 'y': MAP_SIZE})).should.be(True)
+
+        with and_.the_rover_should_wrap_from_the_left_edge:
+            rover.set_orders('lffff')
+            rover.run_orders()
+            print(rover.get_position())
+            the(is_same_dictionary(rover.get_position(), {'x': MAP_SIZE, 'y': MAP_SIZE})).should.be(True)
+
+        with and_.the_rover_should_wrap_from_the_right_edge:
+            rover.set_orders('rrf')
+            rover.run_orders()
+            print(rover.get_position())
+            the(is_same_dictionary(rover.get_position(), {'x': -MAP_SIZE, 'y': MAP_SIZE})).should.be(True)
+
